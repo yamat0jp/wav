601e05c84eee76060b0d64390a7334590acf095b
yamat0jp <yamat0jp@yahoo.co.jp>
2018-05-19 16:44:58 +0900

    確信に使づいてきました
    今日は行楽日和ですけどやってます

--- effect.pas
+++ effect.pas
@@ -6,6 +6,7 @@ uses System.Classes, System.SysUtils, Math, spWav;
 
 function effect8BitWav(const sp: SpParam): integer;
 function effect16BitWav(const sp: SpParam): integer;
+function sinc(x: Single): Single;
 procedure usage;
 function effectwav(const sp: SpParam): integer;
 
@@ -13,43 +14,10 @@ implementation
 
 function effect8BitWav(const sp: SpParam): integer;
 const
-  depth = 1.0;
-  rate = 170.0;
-var
-  i, delayStart: integer;
-  k, m: Single;
-  pMem, pCpy: array of Byte;
-  s: TMemoryStream;
-begin
-  result := 0;
-  try
-    s := TMemoryStream.Create;
-    s.Write(sp.pWav^, sp.sizeOfData);
-    s.Position := 0;
-    s.Read(Pointer(pCpy)^, sp.sizeOfData);
-    pMem := sp.pWav;
-    i := sp.posOfData;
-    k := 8 * sp.sizeOfData / sp.bitsPerSample;
-    while i < k do
-    begin
-      m := depth * sin(2 * pi * rate / sp.samplePerSec);
-      pMem[i + 0] := trunc(m * pMem[i + 0]) + 128;
-      pMem[i + 1] := trunc(m * pMem[i + 1]) + 128;
-      inc(i, 2);
-    end;
-  except
-    result := -1;
-  end;
-  s.Free;
-  Finalize(pCpy);
-end;
-
-function effect16BitWav(const sp: SpParam): integer;
-const
   j = 24;
 var
   i, a, b, pmin, pmax, temp_size, offset0, offset1, p, q: integer;
-  k, m, max, pitch, rate: Single;
+  k, m, ma, pitch, rate: Single;
   pMem, pCpy: array of SmallInt;
   r: array of Single;
   s: TMemoryStream;
@@ -64,35 +32,35 @@ begin
     pMem := sp.pWav;
     i := sp.posOfData;
     k := 8 * sp.sizeOfData / sp.bitsPerSample;
-    temp_size := trunc(sp.samplePerSec * 0.01);
-    pmin := trunc(sp.samplePerSec * sp.bitsPerSample * 0.005);
-    pmax := trunc(sp.samplePerSec * sp.bitsPerSample * 0.02);
+    temp_size := trunc(sp.samplePerSec * sp.bitsPerSample * sp.channels * 0.01);
+    pmin := trunc(sp.samplePerSec * sp.bitsPerSample * sp.channels * 0.005);
+    pmax := trunc(sp.samplePerSec * sp.bitsPerSample * sp.channels * 0.02);
     SetLength(r, pmax - pmin);
     offset0 := sp.posOfData;
     offset1 := sp.posOfData;
     rate := 0.66;
     while offset0 + pmax * 2 < sp.sizeOfData do
     begin
-      max := 0.0;
+      ma := 0.0;
       p := pmin;
-      for b := pmin to pmax do
+      for b := 0 to pmax - pmin - 1 do
       begin
         r[b] := 0.0;
         for a := sp.posOfData to sp.posOfData + temp_size do
-          r[b] := r[b] + pMem[a] * pCpy[a + b];
-        if r[b] > max then
+          r[b] := r[b] + pCpy[a] * pCpy[a + b];
+        if r[b] > ma then
         begin
-          max := r[b];
+          ma := r[b];
           p := b;
         end;
       end;
       for i := 0 to p - 1 do
       begin
-        pMem[offset1 + i] := trunc(pCpy[offset0 + i]);
+        pMem[offset1 + i] := pCpy[offset0 + i];
         pMem[offset1 + i + p] := trunc(pCpy[offset0 + p + i] * (p - i) / p +
           pCpy[offset0 + i] * i / p);
       end;
-      q := trunc(rate * p / (1.0 - rate) + 0.5);
+      q := trunc(rate * p / Abs(1.0 - rate) + 0.5);
       for i := p to q - 1 do
       begin
         if offset0 + i >= sp.sizeOfData then
@@ -102,14 +70,14 @@ begin
       inc(offset0, q);
       inc(offset1, p + q);
     end;
-    pitch := 0.66;
+    pitch := 1.5;
     for i := sp.posOfData to sp.sizeOfData do
     begin
       m := pitch * i;
       q := trunc(m);
       for a := q - j div 2 to q + j div 2 do
         if (m >= sp.posOfData) and (m < sp.sizeOfData) then
-          pMem[a] := trunc(pMem[a + 0] + pCpy[a + 0] * ArcSin(pi * (m - a)));
+          pMem[a] := trunc(pMem[a + 0] + pCpy[a + 0] * sinc(pi * (m - a)));
     end;
   except
     result := -1;
@@ -117,6 +85,89 @@ begin
   Finalize(r);
   s.Free;
   Finalize(pCpy);
+end;
+
+function effect16BitWav(const sp: SpParam): integer;
+const
+  j = 24;
+var
+  i, a, b, pmin, pmax, temp_size, offset0, offset1, p, q: integer;
+  k, m, ma, pitch, rate: Single;
+  pMem, pCpy: array of SmallInt;
+  r: array of Single;
+  s: TMemoryStream;
+begin
+  result := 0;
+  s := TMemoryStream.Create;
+  try
+    SetLength(pCpy, sp.sizeOfData);
+    s.Write(sp.pWav^, sp.sizeOfData);
+    s.Position := 0;
+    s.Read(Pointer(pCpy)^, sp.sizeOfData);
+    pMem := sp.pWav;
+    i := sp.posOfData;
+    k := 8 * sp.sizeOfData / sp.bitsPerSample;
+    temp_size := trunc(sp.samplePerSec * sp.bitsPerSample * sp.channels * 0.01);
+    pmin := trunc(sp.samplePerSec * sp.bitsPerSample * sp.channels * 0.005);
+    pmax := trunc(sp.samplePerSec * sp.bitsPerSample * sp.channels * 0.02);
+    SetLength(r, pmax - pmin);
+    offset0 := sp.posOfData;
+    offset1 := sp.posOfData;
+    rate := 0.66;
+    while offset1 + i + p < sp.sizeOfData do
+    begin
+      ma := 0.0;
+      p := pmin;
+      for b := 0 to pmax - pmin - 1 do
+      begin
+        r[b] := 0.0;
+        for a := sp.posOfData to sp.posOfData + temp_size do
+          r[b] := r[b] + pCpy[a] * pCpy[a + b];
+        if r[b] > ma then
+        begin
+          ma := r[b];
+          p := b;
+        end;
+      end;
+      for i := 0 to p - 1 do
+      begin
+        pMem[offset1 + i] := pCpy[offset0 + i];
+        pMem[offset1 + i + p] := trunc(pCpy[offset0 + p + i] * (p - i) / p +
+          pCpy[offset0 + i] * i / p);
+      end;
+      q := trunc(rate * p / Abs(1.0 - rate) + 0.5);
+      for i := p to q - 1 do
+      begin
+        if offset0 + i >= sp.sizeOfData then
+          break;
+        pMem[offset1 + p + i] := pCpy[offset0 + i];
+      end;
+      inc(offset0, q);
+      inc(offset1, p + q);
+    end;
+    pitch := 1.5;
+    for i := sp.posOfData to sp.sizeOfData do
+    begin
+      m := pitch * i;
+      q := trunc(m);
+      for a := q - j div 2 to q + j div 2 do
+        if (a >= sp.posOfData) and (a < sp.sizeOfData) then
+          pMem[i] := pMem[i] + pCpy[a] * trunc(sinc(pi * (m - a)));
+    end;
+  except
+    result := -1;
+  end;
+  Finalize(r);
+  s.Free;
+  Finalize(pCpy);
+end;
+
+function sinc(x: Single): Single;
+begin
+  if x = 0 then
+    result := 1
+  else
+    result := sin(x) / x;
 end;
 
 procedure usage;
