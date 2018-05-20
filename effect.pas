3ba28cc7b7587ab1763c560d017693a3f9476987
yamat0jp <yamat0jp@yahoo.co.jp>
2018-05-20 10:14:53 +0900

    コードがちょっと短く
    音声の後ろが切れる問題は解決できませんでした。

--- effect.pas
+++ effect.pas
@@ -13,76 +13,7 @@ function effectwav(const sp: SpParam): integer;
 implementation
 
 function effect8BitWav(const sp: SpParam): integer;
-const
-  j = 24;
-var
-  i, a, b, pmin, pmax, temp_size, offset0, offset1, p, q: integer;
-  k, m, ma, pitch, rate: Single;
-  pMem, pCpy: array of SmallInt;
-  r: array of Single;
-  s: TMemoryStream;
 begin
-  result := 0;
-  s := TMemoryStream.Create;
-  try
-    SetLength(pCpy, sp.sizeOfData);
-    s.Write(sp.pWav^, sp.sizeOfData);
-    s.Position := 0;
-    s.Read(Pointer(pCpy)^, sp.sizeOfData);
-    pMem := sp.pWav;
-    temp_size := trunc(sp.samplePerSec * sp.bitsPerSample * sp.channels * 0.01);
-    pmin := trunc(sp.samplePerSec * sp.bitsPerSample * sp.channels * 0.005);
-    pmax := trunc(sp.samplePerSec * sp.bitsPerSample * sp.channels * 0.02);
-    SetLength(r, pmax - pmin);
-    offset0 := sp.posOfData;
-    offset1 := sp.posOfData;
-    rate := 0.66;
-    while offset0 + pmax * 2 < sp.sizeOfData do
-    begin
-      ma := 0.0;
-      p := pmin;
-      for b := 0 to pmax - pmin - 1 do
-      begin
-        r[b] := 0.0;
-        for a := sp.posOfData to sp.posOfData + temp_size do
-          r[b] := r[b] + pCpy[a] * pCpy[a + b];
-        if r[b] > ma then
-        begin
-          ma := r[b];
-          p := b;
-        end;
-      end;
-      for i := 0 to p - 1 do
-      begin
-        pMem[offset1 + i] := pCpy[offset0 + i];
-        pMem[offset1 + i + p] := trunc(pCpy[offset0 + p + i] * (p - i) / p +
-          pCpy[offset0 + i] * i / p);
-      end;
-      q := trunc(rate * p / Abs(1.0 - rate) + 0.5);
-      for i := p to q - 1 do
-      begin
-        if offset0 + i >= sp.sizeOfData then
-          break;
-        pMem[offset1 + p + i] := pCpy[offset0 + i];
-      end;
-      inc(offset0, q);
-      inc(offset1, p + q);
-    end;
-    pitch := 1.5;
-    for i := sp.posOfData to sp.sizeOfData do
-    begin
-      m := pitch * i;
-      q := trunc(m);
-      for a := q - j div 2 to q + j div 2 do
-        if (m >= sp.posOfData) and (m < sp.sizeOfData) then
-          pMem[a] := trunc(pMem[a + 0] + pCpy[a + 0] * sinc(pi * (m - a)));
-    end;
-  except
-    result := -1;
-  end;
-  Finalize(r);
-  s.Free;
-  Finalize(pCpy);
 end;
 
 function effect16BitWav(const sp: SpParam): integer;
@@ -93,15 +24,9 @@ var
   m, ma, pitch, rate: Single;
   pMem, pCpy: array of SmallInt;
   r: array of Single;
-  s: TMemoryStream;
 begin
   result := 0;
-  s := TMemoryStream.Create;
   try
-    SetLength(pCpy, sp.sizeOfData);
-    s.Write(sp.pWav^, sp.sizeOfData);
-    s.Position := 0;
-    s.Read(Pointer(pCpy)^, sp.sizeOfData);
     pMem := sp.pWav;
     temp_size := trunc(sp.samplePerSec * sp.bitsPerSample * sp.channels * 0.01);
     pmin := trunc(sp.samplePerSec * sp.bitsPerSample * sp.channels * 0.005);
@@ -110,12 +35,13 @@ begin
     offset0 := sp.posOfData;
     offset1 := sp.posOfData;
     rate := 0.66;
+    SetLength(pCpy, sp.sizeOfData);
     k := (sp.sizeOfData - sp.posOfData) div sp.channels;
     for b := 0 to pmax - pmin - 1 do
     begin
       r[b] := 0.0;
       for a := sp.posOfData to sp.posOfData + temp_size do
-        r[b] := r[b] + pCpy[a] * pCpy[a + b];
+        r[b] := r[b] + pMem[a] * pMem[a + b];
       if r[b] > ma then
       begin
         ma := r[b];
@@ -128,16 +54,16 @@ begin
       p := pmin;
       for i := 0 to p do
       begin
-        pMem[offset1 + i] := pCpy[offset0 + i];
-        pMem[offset1 + i + p] := trunc(pCpy[offset0 + p + i] * (p - i) / p +
-          pCpy[offset0 + i] * i / p);
+        pCpy[offset1 + i] := pMem[offset0 + i];
+        pCpy[offset1 + i + p] := trunc(pMem[offset0 + p + i] * (p - i) / p +
+          pMem[offset0 + i] * i / p);
       end;
       q := trunc(rate * p / (1.0 - rate) + 0.5);
       for i := p to q - 1 do
       begin
         if offset1 + i + p >= k then
           break;
-        pMem[offset1 + p + i] := pCpy[offset0 + i];
+        pCpy[offset1 + p + i] := pMem[offset0 + i];
       end;
       inc(offset0, q);
       inc(offset1, p + q);
@@ -149,7 +75,7 @@ begin
       q := trunc(m);
       for a := q - j div 2 to q + j div 2 do
         if (a >= sp.posOfData) and (a < k) then
-          pMem[i] := pMem[a] + pCpy[a] * trunc(sinc(pi * (m - a)))
+          pMem[i] := pCpy[a] + pMem[a] * trunc(sinc(pi * (m - a)))
         else
           pMem[i] := 0;
     end;
@@ -158,7 +84,6 @@ begin
   end;
   Finalize(pCpy);
   Finalize(r);
-  s.Free;
 end;
 
 function sinc(x: Single): Single;
